8f4a8b1aadeead7e3b9ae1d16757e982ee617310
Clara Luis Mingueza <69794212+clm-empatia@users.noreply.github.com>
2022-04-01 14:59:31 +0200

    Upload files from Methodology Part 1

---
+++ extract_corpus_bbddlab.py
@@ -0,0 +1,142 @@
+import os
+import glob
+import pandas as pd
+import numpy as np
+import difflib
+import json
+
+# Variables
+in_path = '/home/vocalyre/ws/empatia/interspeech_aasc_tfidf/yamnet_outputs/uc3m4safety_stimulus/binscores/'
+out_path = '/home/vocalyre/ws/empatia/interspeech_aasc_tfidf/corpus/uc3m4safety_stimulus/'
+mid_parents = ['/m/04rlf', '/m/09x0r', '/m/068hy',
+               '/m/0ch8v', '/m/0ghcn6', '/m/0kpv1t',
+               '/t/dd00028']  # Musical instrument, Music genre, Music role, Domestic animals, pets, Livestock, farm animals, working animals. Musical ensemble excluded (not in YAMNet)
+
+
+def main():
+    id_df = pd.read_csv(in_path + 'class_labels_indices.csv')  # Read ids csv
+    mids_corpus = []
+    n_file = 0
+    # Read Audioset Ontology file:
+    with open('/home/vocalyre/ws/empatia/tfidf_fear/ontology_noblacklist.json', 'r') as f:
+        audioset_ontology = json.load(f)
+
+    # TASK 1: From a mid parent list extract str children names
+    str_children = []
+
+    for ontology_object in audioset_ontology:
+        for parent_mid in mid_parents:
+            # print('Parent: ' + parent_mid)
+            # print(ontology_object['id'])
+            if parent_mid == ontology_object['id']:
+                mid_children = ontology_object['child_ids']  # List
+                # print(mid_children)
+                for mid_child in mid_children:
+                    str_child = id_df.loc[id_df['mid'] == str(mid_child), 'display_name']  # Convert to string
+                    if str_child.values.size > 0:  # If exists (not empty position), append to drop afterwards
+                        # print(str_child.values[0])
+                        str_children.append(str_child.values[0])
+                    # mid_children.append(mid_child)
+
+    print(str_children)
+    # print(mid_children)
+
+    for ontology_object in audioset_ontology:
+        for child_str in str_children:
+            child_mid = id_df.loc[id_df['display_name'] == str(child_str), 'mid']  # Convert to mid
+            # print(child_mid.values[0])
+            if child_mid.values[0] == ontology_object['id']:
+                mid_children_children = ontology_object['child_ids']  # List
+                for mid_child in mid_children_children:
+                    str_child = id_df.loc[id_df['mid'] == str(mid_child), 'display_name']  # Convert to string
+                    if str_child.values.size > 0:  # If exists (not empty position), append to drop afterwards
+                        str_children.append(str_child.values[0])
+
+    print(str_children)
+    # print(mid_children)
+
+    # TASK 2: For each .csv in folder, extract corpus:
+    for file_name in sorted(glob.glob(os.path.join(in_path, '*_binary_logscores_th0027.csv'))):  # BBDD_LAB ALL
+        n_file += 1
+        input_df = pd.read_csv(file_name)
+        file_name = file_name.replace(in_path, '')
+        print(file_name)
+
+        filtered_input_df = input_df.copy()
+        for child in str_children:
+            if child in filtered_input_df.columns:
+                if id_df['display_name'].str.contains(child).any():
+                    # filtered_input_df = input_df.drop(columns=str_children)  # Drop children columns
+                    filtered_input_df = filtered_input_df.drop([child], axis=1)  # Drop children columns
+            # print('Dropped: ' + str(child))
+        # print('Dropped labels: Musical instrument, Music genre, Music role, Domestic animals, pets, Livestock, farm animals, working animals, Musical ensemble, ' + str(str_children))
+        print('After filtering shape:' + str(np.shape(filtered_input_df)))
+
+        mids_list = []
+        # Convert binary matrix to mid and zeros matrix:
+        for label_str in list(filtered_input_df.columns):
+            # print(label_str)
+            # Match strings from input and mid indices dataframes
+            match = difflib.get_close_matches(str(label_str), list(id_df['display_name']), n=1, cutoff=0.8)
+            mid_series = id_df.loc[id_df['display_name'] == ''.join(match), 'mid']
+            mid_str = mid_series.to_string().split("/", 1)[1]  # get content only
+            # Add MID to every 1 position in the column
+            filtered_input_df.loc[filtered_input_df[label_str] == 1, label_str] = '/' + str(mid_str)
+
+        # For each row, append mids to create the corpus
+        for index, row in filtered_input_df.iterrows():
+            mids_str = row.values.tolist()
+            mids_str = [i for i in mids_str if i != 0]
+            print(mids_str)
+            mids_strstr = ' '.join(mids_str)  # list of one row to string
+            # mids_row_list = [mids_strstr, str(n_file)]
+
+            mids_row_list = [mids_strstr, str(n_file)]  # list of one row to string with file ref
+            mids_list.append(mids_row_list)  # list of all rows
+
+        # mids_row_list = ' '.join(mids_row_list)  # list of all rows to string
+        # mids_list = [mids_row_list, str(n_file)]
+        print(mids_list)
+        mids_corpus.append(mids_list)
+
+        #     # label_str = id_df.loc[id_df['display_name'] == str(label_str)]['display_name']
+        #     mids_str.append('/' + str(mid_str) + ',')
+        #     # print(str(mids_str))
+        #     # labels_str = labels_str.join(label_str) + ', '
+        #     labels_str.append(str(label_str) + ',')
+        #     # print(labels_str)
+        #
+        # mids_strstr = ' '.join(mids_str)  # list to string
+        # labels_strstr = ' '.join(labels_str)
+        #
+        # # labels_corpus.append(labels_list)
+        # mids_list = [mids_strstr, str(n_file)]
+        # labels_list = [labels_strstr, str(n_file)]
+        # mids_corpus.append(mids_list)
+        # labels_corpus.append(labels_list)
+        #
+        # #####
+        #
+        # # Filter all zero, inf or nan columns
+        # log_scores = log_scores.replace([np.inf, -np.inf, np.nan], 0)
+        #
+        # # Filter any column that does not present a single value above the threshold
+        # filtered_log_scores = log_scores.loc[:, (log_scores >= THR).any(axis=0)]
+        # print(filtered_log_scores.head())
+    print('After filtering shape:' + str(np.shape(filtered_input_df)))
+    # filtered_input_df.to_csv(path_or_buf=in_path + 'bbddlabstimulus_filtered_220325_logscores.csv', index=False)
+    print('Saved\n-----\n')
+    # print(np.shape(labels_corpus))
+    # print(labels_corpus)
+    # Save corpus
+    # file = open(out_path + "corpus_mids_220401_bbddlab_allemotions_all_patchhop_1.txt", "w")  # BBDD_LAB (ALL)
+    # file.write(str(mids_corpus))
+    # file.close()
+
+    # with open(out_path + "corpus_mids_220401_bbddlab_allemotions_all_patchhop_1.csv", "w") as f:  # BBDD_LAB (ALL)
+    #     wr = csv.writer(f)
+    #     wr.writerows(mids_corpus)
+
+
+if __name__ == '__main__':
+    main()
---
+++ scores_to_binary_bbddlab.py
@@ -0,0 +1,55 @@
+import os
+import glob
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import Binarizer
+
+# Variables
+in_path = '/home/vocalyre/ws/empatia/interspeech_aasc_tfidf/yamnet_outputs/uc3m4safety_stimulus/'
+out_path = '/home/vocalyre/ws/empatia/interspeech_aasc_tfidf/yamnet_outputs/uc3m4safety_stimulus/binscores/'
+
+N_CLASSES = 521
+N_SEC = 91
+THR = 0.0027
+
+
+def main():
+
+    median_array = np.zeros([N_CLASSES, 1], dtype='f')
+    mean_array = np.zeros([N_CLASSES, 1], dtype='f')
+    percentile_array = np.zeros([N_CLASSES, 1], dtype='f')
+
+    for file_name in glob.glob(os.path.join(in_path, '*_allscores_patchhop_1.csv')):
+        input_df = pd.read_csv(file_name)
+        file_name = file_name.replace(in_path, '').replace('_allscores_patchhop_1.csv', '')
+        print(file_name)
+
+        log_scores = input_df.transform(lambda x: np.log2(1 + x))
+
+        median_scores = log_scores.median(axis=0).to_numpy().reshape((N_CLASSES, 1))
+        mean_scores = log_scores.mean(axis=0).to_numpy().reshape((N_CLASSES, 1))
+        percentile_scores = log_scores.quantile(q=0.75, axis=0).to_numpy().reshape((N_CLASSES, 1))
+
+        scores_array = log_scores.values
+        transformer = Binarizer(threshold=THR).fit(scores_array)  # fit does nothing.
+        bin_scores = transformer.transform(scores_array)
+        bin_df = pd.DataFrame(data=bin_scores, columns=log_scores.columns)
+        bin_df = bin_df.replace([np.inf, -np.inf, np.nan], 0)
+        bin_df.to_csv(path_or_buf=out_path + file_name + '_binary_logscores_th0027.csv', index=False)
+        print('Saved\n-----\n')
+
+        median_array = np.append(median_array, median_scores, axis=1)
+        mean_array = np.append(mean_array, mean_scores, axis=1)
+        percentile_array = np.append(percentile_array, percentile_scores, axis=1)
+
+    median_array = median_array[1:]
+    mean_array = mean_array[1:]
+    percentile_array = percentile_array[1:]
+
+    print('Median:\n' + str(np.mean(median_array)))  # 0.001179373284057722
+    print('\nMean:\n' + str(np.mean(mean_array)))  # 0.0026679743814952746
+    print('\nPercentile 75:\n' + str(np.mean(percentile_array)))  # 0.002307613669274959
+
+
+if __name__ == '__main__':
+    main()
